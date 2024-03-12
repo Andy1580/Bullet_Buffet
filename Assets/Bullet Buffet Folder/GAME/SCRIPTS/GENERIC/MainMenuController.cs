bd1c533ba279034914b4b4a9400ef921f9b5d65a
Andres Lopez <andreslopezch1996@gmail.com>
2024-03-12 00:04:33 -0600

    Adelanto del modo de captura la bandera

--- Assets/Bullet Buffet Folder/GAME/SCRIPTS/GENERIC/MainMenuController.cs
+++ Assets/Bullet Buffet Folder/GAME/SCRIPTS/GENERIC/MainMenuController.cs
@@ -39,7 +39,12 @@ public class MainMenuController : MonoBehaviour
 
     public void ModoEnfrentamiento()
     {
-        SceneManager.LoadScene("ANDY");
+        SceneManager.LoadScene("ANDYCLASH");
+    }
+
+    public void ModeCaptureFlag()
+    {
+        SceneManager.LoadScene("ANDYFLAG");
     }
 
 
